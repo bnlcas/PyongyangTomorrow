065430bc55714ce43607d5b6310b8493fb93ada3
Benjamin Lucas <bnlcas@gmail.com>
2024-11-23 22:45:34 -0500

    add shader debug view

--- PyongyangTomorrow/TestView.swift
+++ PyongyangTomorrow/TestView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct TestView: View {
+    @State var fadeInterval = 0.5
+    let startDate = Date()
+    //
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                .fill(.gray)
+                .frame(width:400, height:300)
+                .visualEffect { content, proxy in
+                    content
+                        .colorEffect(ShaderLibrary.burnTransition(
+                            .float(fadeInterval),
+                            .float2(proxy.size)
+                        ))
+                }
+                .onTapGesture  {
+                    fadeInterval = 0.0
+                    withAnimation(.linear(duration: 2.2)){
+                        fadeInterval = 1.0
+                    }
+                }
+            //.colorEffect(ShaderLibrary.burnTransition(.float(0.7)))
     }
 }
 
